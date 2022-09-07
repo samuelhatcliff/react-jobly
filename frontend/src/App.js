a9b250b4db50999a0664ff9ba9edee282b03446d
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-09-06 18:33:22 -0600

    added react router routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,33 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
 import NavBar from './navBar.js';
 import Company from './company.js';
 import Job from './job.js';
 import Profile from './profile.js';
+import SignIn from './signIn.js';
+import SignUp from './signUp.js';
 
 
 function App() {
-  const [signedIn, setSignedIn] = useState(false);
+  const [signedIn, setSignedIn] = useState(true);
   return (
     <div>
-      <BrowserRouter>
-        <Route exact path="/companies">
-          <Company />
+      <Routes>
+        <Route path="/companies" element={<Company />}>
         </Route>
-        <Route exact path="/jobs">
-          <Job />
+        <Route path="/jobs" element={<Job />}>
         </Route>
-        <Route exact path="/profile">
-          <Profile />
+        <Route path="/profile" element={<Profile />}>
         </Route>
-        <Route exact path="/logout">
+        <Route path="/logout">
         </Route>
-        <Route exact path="/signup">
-          <signUp />
+        <Route path="/signup" element={<SignUp />}>
         </Route>
-        <Route exact path="/signin">
-          <signIn />
+        <Route path="/signin" element={<SignIn />}>
         </Route>
+      </Routes>
+      <NavBar signedIn={signedIn} />
 
-        <NavBar signedIn={signedIn} />
-      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/navBar.js
+++ frontend/src/navBar.js
@@ -5,23 +5,23 @@ const NavBar = ({ signedIn }) => {
 
     return (
         <nav>
-            <AppBar position="fixed" className="appBar">
-                <Toolbar className="navigation">
-                    {signedIn ? (
-                        <>
-                            <NavLink exact to="/companies">Companies</NavLink>
-                            <NavLink exact to="/jobs">Jobs</NavLink>
-                            <NavLink exact to="/profile">Profile</NavLink>
-                            <NavLink exact to="/logout">Log Out</NavLink>
-                        </>
-                    ) : (
-                        <>
-                            <NavLink exact to="/login">Sign in</NavLink>
-                            <NavLink exact to="/signup">Sign up</NavLink>
-                        </>
-                    )}
-                </Toolbar>
-            </AppBar>
+            {/* <AppBar position="fixed" className="appBar">
+                <Toolbar className="navigation"> */}
+            {signedIn ? (
+                <>
+                    <NavLink exact to="/companies">Companies</NavLink>
+                    <NavLink exact to="/jobs">Jobs</NavLink>
+                    <NavLink exact to="/profile">Profile</NavLink>
+                    <NavLink exact to="/logout">Log Out</NavLink>
+                </>
+            ) : (
+                <>
+                    <NavLink exact to="/signin">Sign in</NavLink>
+                    <NavLink exact to="/signup">Sign up</NavLink>
+                </>
+            )}
+            {/* </Toolbar>
+            </AppBar> */}
         </nav>
     )
 }
