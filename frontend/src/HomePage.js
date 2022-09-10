631b488d509318d0adfa8c4535e924c3bb08a7c9
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-09-10 15:27:47 -0600

    made homepage dynamci

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,8 +1,17 @@
+import { useState, useContext } from 'react';
+import UserContext from './user/UserContext';
+
+
+
 const HomePage = () => {
+    const { user } = useContext(UserContext)
     return (
         <div>
-            <h1>Welcome to the homepage!</h1>
+            <h1>Jobly</h1>
+            <h3>All the jobs in one, convenient place.</h3>
+            {user.length ? (<><h5>Welcome Back {user[0]}!</h5></>) : (<><h5>Please sign in or create an account to use the platform!</h5></>)}
         </div>
     )
+
 }
 export default HomePage
